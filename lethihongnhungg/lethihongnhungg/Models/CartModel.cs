899a0643d0a5c35d771697de3411de0059ec1311
hongnhungle <hongnhung0104@gmail.com>
2024-08-14 11:17:24 +0700

    first commit

---
+++ lethihongnhungg/lethihongnhungg/Models/CartModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using lethihongnhungg.DB;
+
+namespace lethihongnhungg.Models
+{
+    public class CartModel
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
